806d82c1ddd96bf57aeee287b6544457811ea03e
Jim Kang <jimkang@gmail.com>
2018-01-26 20:41:20 -0500

    Post to archive.

--- autocompleterap.js
+++ autocompleterap.js
@@ -11,6 +11,24 @@ var getReliableTemplate = require('./getreliabletemplate');
 var Twit = require('twit');
 var callNextTick = require('call-next-tick');
 
+var StaticWebArchiveOnGit = require('static-web-archive-on-git');
+var queue = require('d3-queue').queue;
+var randomId = require('idmaker').randomId;
+
+var staticWebStream = StaticWebArchiveOnGit({
+  config: config.github,
+  title: config.archiveName,
+  footerScript: `<script type="text/javascript">
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-49491163-1', 'jimkang.com');
+  ga('send', 'pageview');
+</script>`,
+  maxEntriesPerPage: 50
+});
+
 var dryRun = false;
 
 if (process.argv.length > 2) {
@@ -49,22 +67,40 @@ function postPairRap(error, rap) {
   } else {
     console.log(rap);
     if (!dryRun) {
-      twit.post(
-        'statuses/update',
-        {
-          status: rap
-        },
-        function done(twitterError, data) {
-          if (twitterError) {
-            console.log(twitterError);
-            console.log('data:', data);
-          } else {
-            console.log('Posted to Twitter.');
-          }
-        }
-      );
+      postToTargets(rap, logError);
     }
   }
+}
+
+function logError(error, data) {
+  if (error) {
+    console.log(error);
+    console.log('data:', data);
+  }
+}
+
+function postToTargets(text, done) {
+  var q = queue();
+  q.defer(postTweet, text);
+  q.defer(postToArchive, text);
+  q.await(done);
+}
+
+function postTweet(text, done) {
+  var body = {
+    status: text
+  };
+  twit.post('statuses/update', body, done);
+}
+
+function postToArchive(text, done) {
+  var id = 'rap-' + randomId(8);
+  staticWebStream.write({
+    id,
+    date: new Date().toISOString(),
+    caption: text
+  });
+  staticWebStream.end(done);
 }
 
 function getUnvettedTemplate(done) {
